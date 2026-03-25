2eab5846b662db0f5a16ad4617be1c67e9924c0d
NOTHING-R <kalilinux.nothing@gmail.com>
2026-03-26 02:54:52 +0600

    added style and products

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,7 @@
+function Auth(){
+    return <div>
+        <h1>Auth Page</h1>
+    </div>  
+}
+
+export default Auth 
---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,7 @@
+function Checkout(){
+    return <div>
+        <h1>Checkout Page</h1>
+    </div>  
+}
+
+export default Checkout 
